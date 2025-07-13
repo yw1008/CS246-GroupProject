aa3fbc46b9d3c52eac731d5fb310dfbb9880f8fe
Yewoon Jung <yewoon1008@gmail.com>
2025-07-13 18:57:24 -0400

    working on the main

--- main.cc
+++ main.cc
@@ -8,6 +8,7 @@ using namespace std;
 int main() {
     string cmd;
     Board b;
+    bool isSetup = 0;
 
     while(true) {
         cin >> cmd;
@@ -19,8 +20,11 @@ int main() {
             cin >> blackP;
 
             b.init();
-            b.defBoard();
-            ...
+            if(isSetup){
+
+            } else {
+                b.defBoard();
+            }
 
             cout << b; // print the board
         } else if(cmd == "resign") {
@@ -32,19 +36,20 @@ int main() {
         } else if(cmd == "setup") {
             string setupCmd;
 
-            while(cin >> setupCmd == "done") {
+            while(cin >> setupCmd != "done") {
                 cin >> setupCmd;
 
-                if(setupCmd == "+") {
+                if(setupCmd == "+") { // 
                     ...
-                }else if(setupCmd == "-") {
+                }else if(setupCmd == "-") { // setup command is -, which is remove the piece at the board
                     ...
-                } else if(setupCmd == "=") {
+                } else if(setupCmd == "=") { // setup command is =, which is change the turn
                     ...
                 } else {
                     ... // Do we need to consider when the user use invalide command for setup? (e.g. other than +, -, =, and done)
                 }
             }
+            isSetup += 1;
         } else if(cmd == "move") {
             string from;
             string to;
