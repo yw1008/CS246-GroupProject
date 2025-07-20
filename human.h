c7bb2286f2661b0ce07b2ff3c52bd915793f9399
stevecentral <stefan.ticu10@gmail.com>
2025-07-20 16:54:12 -0400

    human class

---
+++ human.h
@@ -0,0 +1,19 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include "player.h"
+#include "board.h"
+
+class Human : public Player {
+public:
+    // Constructor
+    Human(bool isWhite);
+
+    // Perform a move 
+    void move(Game &game);
+    
+    // Destructor
+    ~Human() override;
+};
+
+#endif
