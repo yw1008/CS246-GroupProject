1ff3d656a8b094bb53b0f848d37bce8676170694
jhannah0711 <jjhannah0711@gmail.com>
2025-07-20 16:18:13 -0400

    edit text display

--- textdisplay.cc
+++ textdisplay.cc
@@ -1,36 +1,54 @@
 #include "textdisplay.h"
 #include <iostream>
 
-TextDisplay::TextDisplay() {
-    for (std::size_t r = 0; r < boardSize; ++r) {
-        for (std::size_t c = 0; c < boardSize; ++c) {
-            if (r%2 == c%2) theBoard[r][c].type = '_'; //black cell
-            else theBoard[r][c].type = ' '; //white cell
+using namespace std;
+
+TextDisplay::TextDisplay()
+    : theTD(BOARD_SIZE, vector<char>(BOARD_SIZE)) {
+    for (size_t r = 0; r < BOARD_SIZE; ++r) {
+        for (size_t c = 0; c < BOARD_SIZE; ++c) {
+            theTD[r][c] = (r % 2 == c % 2) ? '_' : ' ';
         }
     }
 }
 
-void notify(Subject &whoNotified) override {
+void notify(Subject &whoNotified) {
     // need end position and the type of the piece (with color) that is moved. 
     // for now just assume each variable represent those two
-    std::vecotr<std::size_t> endPos;
-    char piece;
-    std::size_t r = endPos[0], c = endPos[1];
-    theTD[r][c] = piece;
-    // also need to update caught piece?
+    Info info = whoNotified.getInfo();
+    Colour c = info.colour;
+    pieceType type = info.pieceType;
+    if (c == Colour::White) {
+        if (type == pieceType::King) theTD[info.pos.row][info.pos.col] = 'K';
+        else if (type == pieceType::Queen) theTD[info.pos.row][info.pos.col] = 'Q';
+        else if (type == pieceType::Rook) theTD[info.pos.row][info.pos.col] = 'R';
+        else if (type == pieceType::Knight) theTD[info.pos.row][info.pos.col] = 'N';
+        else if (type == pieceType::Bishop) theTD[info.pos.row][info.pos.col] = 'B';
+        else if (type == pieceType::Pawn) theTD[info.pos.row][info.pos.col] = 'P';
+        else theTD[info.pos.row][info.pos.col] = ' ';
+    }
+    else {
+        if (type == pieceType::King) theTD[info.pos.row][info.pos.col] = 'k';
+        else if (type == pieceType::Queen) theTD[info.pos.row][info.pos.col] = 'q';
+        else if (type == pieceType::Rook) theTD[info.pos.row][info.pos.col] = 'r';
+        else if (type == pieceType::Knight) theTD[info.pos.row][info.pos.col] = 'n';
+        else if (type == pieceType::Bishop) theTD[info.pos.row][info.pos.col] = 'b';
+        else if (type == pieceType::Pawn) theTD[info.pos.row][info.pos.col] = 'p';
+        else theTD[info.pos.row][info.pos.col] = '_';
+    }
 }
 
-std::ostream &operator<<(std::ostream &out, const TextDisplay &td) {
-    std::size_t colNum = boardSize;
-    for (std::size_t r = 0; r < boardSize; ++r) {
+ostream &operator<<(ostream &out, const TextDisplay &td) {
+    size_t colNum = BOARD_SIZE;
+    for (size_t r = 0; r < BOARD_SIZE; ++r) {
         out << colNum << " ";
         --colNum;
-        for (std::size_t c = 0; c < boardSize; ++c) {
+        for (size_t c = 0; c < BOARD_SIZE; ++c) {
             out << td[r][c];
         }
-        out << std::endl;
+        out << endl;
     }
-    out << std::endl;
-    out << " abcdefgh" << std::endl;
+    out << endl;
+    out << " abcdefgh" << endl;
     return out;
 }
