ee9f9644dd0c3ca0bf79b50ea3949662baf7144c
hj4jeong <hj4jeong@uwaterloo.ca>
2025-07-25 03:12:49 -0400

    fix bug - undo

--- main.cc
+++ main.cc
@@ -163,6 +163,7 @@ int main() {
                                 cout << "Black is in check." << endl;
                             } else {
                                 if(board->isStalemate()){ 
+                                cout << *board << endl;
                                 cout << "Stalemate!" << endl;
                                 whiteP->~Player();
                                 blackP->~Player();
@@ -179,6 +180,8 @@ int main() {
                     }
                     else if (cmd == "undo") {
                         game->undo();
+                        whosTurn = whosTurn == "Black" ? "White" : "Black";
+                        board->changeTurn();
                     }
                     else {
                         cerr << "Invalid input for the game" << endl;
